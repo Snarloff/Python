886434d330b14d10f7de2aac29395dfbfe56d2a7
Snarloff <pnsgraal@gmail.com>
2019-11-22 21:39:41 -0300

    Adicionada a pasta Data

--- register_user.py
+++ register_user.py
@@ -33,25 +33,25 @@ class MakeTable:
 		return len(open("../data/logs.txt", "r").readlines()) 
 
 	def new_user(self, name:str, age:str):
-		with open("../data/logs.txt", "a+") as file:
+		with open("data/logs.txt", "a+") as file:
 			return file.write(str("%s: %s %s\n" %(str(self.len_archives()+1), str(name), str(age))))
 
 	def del_archives(self, num:int):
-		archive = open("../data/logs.txt", "r").readlines() # Retorna uma lista
+		archive = open("data/logs.txt", "r").readlines() # Retorna uma lista
 		archive.pop(num)
-		archive2 = open("../data/logs.txt", "wt")
+		archive2 = open("data/logs.txt", "wt")
 		return archive2.write("".join(map(str, archive)))
 
 	def edit(self, num:int, user:str, age:str):
-		archive = open("../data/logs.txt", "r").readlines()
+		archive = open("data/logs.txt", "r").readlines()
 		archive[num] = "%s: %s %s\n" %(num+1, user, age)
-		archive2 = open("../data/logs.txt", "wt")
+		archive2 = open("data/logs.txt", "wt")
 		return archive2.write("".join(map(str, archive)))		
 
 	def main(self):
 		response = self.menu
 		if response == "1":
-			print("\n%s" %(str(open("../data/logs.txt", "r").read())) + "-=-"*10)
+			print("\n%s" %(str(open("data/logs.txt", "r").read())) + "-=-"*10)
 		elif response == "2":
 			print("-=-"*10)
 			return self.new_user(name=str(input("Nome: ")), age=int(input("Idade: ")))
